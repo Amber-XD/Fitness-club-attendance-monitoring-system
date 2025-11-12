27553b23a875e7725a4e8816c34b346bc0df5e8a
Ihor Holodniuk <mishaholodniuk@gmail.com>
2025-11-12 21:14:57 +0200

    first commit

--- Exam_Project/Exam_Project.cpp
+++ Exam_Project/Exam_Project.cpp
@@ -1,7 +1,21 @@
 
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+
+struct Visit{
+	shared_ptr<Client> client;
+	string date;
+	string notes;
+	Visit(shared_ptr<Client> c, string& d, string& n) : client(c), date(d), notes(n) {}
+};
+
+class Client {
+	
+
+};
 
 int main()
 {
